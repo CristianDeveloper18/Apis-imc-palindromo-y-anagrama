1683ba08b93a8a0c0eed8e95c67fbc0773def232
Cristian <Cristian@LAPTOP-27MN5473>
2021-10-01 23:43:21 -0500

    Agregar archivos de proyecto.

---
+++ ICM/Controllers/IMCControllers.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IMCControllers : ControllerBase
+    {
+
+        [HttpGet]
+
+        public IActionResult IMCResultado(double altura, double peso)
+
+        {
+
+            var R = new ObjetoImc();
+
+            R.Peso = peso;
+
+            R.Altura = altura / 100;
+
+            var AFinal = R.Altura;
+
+            var IMC = peso / (AFinal * AFinal);
+
+            var Clasificacion = "";
+
+
+
+            if (IMC < 18.5)
+
+            {
+
+                Clasificacion = "Tienes un peso inferior a lo normal";
+
+            }
+
+            else
+
+            {
+
+                if (IMC >= 18.5 && IMC <= 24.9)
+
+                {
+
+                    Clasificacion = "Tienes un peso normal";
+
+                }
+
+                else
+
+                {
+
+                    if (IMC >= 25.0 && IMC <= 29.9)
+
+                    {
+
+                        Clasificacion = "Tienes un peso superior al normal";
+
+                    }
+
+                    else
+
+                    {
+
+                        Clasificacion = "Tienes obesidad";
+
+                    }
+
+                }
+
+            }
+
+            var Resultado = "Su IMC es: " + Convert.ToString(IMC) + "y su composicion corporal es:  " + Clasificacion;
+
+            return Ok(Resultado);
+
+        }
+
+    }
+
+}
+    
+
---
+++ ICM/ObjetoImc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICM
+{
+    public class ObjetoImc
+    {
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+    }
+
+}
+
